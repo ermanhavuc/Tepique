3ad5b2b67fa1a9ea7f24bccbd1a6c639d0b922db
ermanhavuc <ermanhavuc@gmail.com>
2019-01-02 17:32:35 +0300

    server now serve at machine's local ip and 80 port,

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 import sys, getopt
-import os
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from socketserver import ThreadingMixIn
 from mimetypes import guess_type
@@ -7,49 +6,29 @@ from mimetypes import guess_type
 class ThreadingSimpleServer(ThreadingMixIn, HTTPServer):
     pass
 
-
 class TepiqueServer(BaseHTTPRequestHandler):
 
     def do_GET(self):
         try:
-            f = open(self.path[1:], "rb")
+            filepath = self.path[1:]
+            if filepath is "" :
+                f = open("index.html", "rb")
+                filetype = "text/html"
+            else:
+                f = open(filepath, "rb")
+                filetype = guess_type(filepath)
             self.send_response(200)
-            self.send_header("Content-Type", guess_type(self.path[1:]))
+            self.send_header("Content-Type", filetype)
             self.end_headers()
             self.wfile.write(f.read())
             f.close()
         except IOError:
             self.send_error(404, "File Not Found")
 
-IP = None
-PORT = None
-
-try:
-    opts, args = getopt.getopt(sys.argv[1:],"hi:p:",["ip=","port="])
-except getopt.GetoptError:
-    print('tepique_server.py -i <ip address> -p <port>')
-    sys.exit(2)
-for opt, arg in opts:
-    if opt == '-h':
-        print('tepique_server.py -i <ip address> -p <port>')
-        sys.exit()
-    elif opt in ("-i", "--ip"):
-        IP = arg
-    elif opt in ("-p", "--port"):
-        PORT = int(arg)
-
-if IP is None:
-    IP = "192.168.0.10"
-if PORT is None:
-    PORT = 8080
-
+IP = "192.168.0.10"
+PORT = 80
 
 server = ThreadingSimpleServer((IP, PORT), TepiqueServer)
-print("Tepique Server running on %s using port %d" %(IP, PORT))
+print("Tepique Server - %s:%d" %(IP, PORT))
 
-try:
-    while 1:
-        sys.stdout.flush()
-        server.handle_request()
-except KeyboardInterrupt:
-    print("\nShutting down server per users request.")
+server.serve_forever()
